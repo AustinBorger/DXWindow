bb6a817e1bc5fa6dadec5637f864a5335170f994
Austin Borger <aaborger@gmail.com>
2015-06-29 14:51:23 -0400

    Added more to IDXWindowCallback

--- DXWindow/DXWindow.h
+++ DXWindow/DXWindow.h
@@ -37,7 +37,53 @@ struct DXWINDOW_DESC {
 };
 
 struct __declspec(uuid("af69111a-a1f8-4cd1-afd3-94abdcfb011c")) IDXWindowCallback : public IUnknown {
-	virtual VOID OnObjectFailure(HRESULT hr) PURE;
+	/* Called when an error has occured anywhere within the API. */
+	virtual VOID STDMETHODCALLTYPE OnObjectFailure(HRESULT hr) PURE;
+
+	//Mouse callbacks
+
+	/* Called when the left mouse button is clicked. */
+	virtual VOID STDMETHODCALLTYPE OnMouseLButtonDown(IDXWindow* pDXWindow, WORD Flags, WORD xPos, WORD yPos) PURE;
+
+	/* Called when the left mouse button is released. */
+	virtual VOID STDMETHODCALLTYPE OnMouseLButtonUp(IDXWindow* pDXWindow, WORD Flags, WORD xPos, WORD yPos) PURE;
+
+	/* Called when the right mouse button is clicked. */
+	virtual VOID STDMETHODCALLTYPE OnMouseRButtonDown(IDXWindow* pDXWindow, WORD Flags, WORD xPos, WORD yPos) PURE;
+
+	/* Called when the right mouse button is released. */
+	virtual VOID STDMETHODCALLTYPE OnMouseRButtonUp(IDXWindow* pDXWindow, WORD Flags, WORD xPos, WORD yPos) PURE;
+
+	/* Called when the middle mouse button is clicked. */
+	virtual VOID STDMETHODCALLTYPE OnMouseMButtonDown(IDXWindow* pDXWindow, WORD Flags, WORD xPos, WORD yPos) PURE;
+
+	/* Called when the middle mouse button is released. */
+	virtual VOID STDMETHODCALLTYPE OnMouseMButtonUp(IDXWindow* pDXWindow, WORD Flags, WORD xPos, WORD yPos) PURE;
+
+	/* Called when the mouse is moved. */
+	virtual VOID STDMETHODCALLTYPE OnMouseMove(IDXWindow* pDXWindow, WORD Flags, WORD xPos, WORD yPos) PURE;
+
+	/* Called when the mouse wheel is moved. */
+	virtual VOID STDMETHODCALLTYPE OnMouseWheel(IDXWindow* pDXWindow, WORD Flags, SHORT Delta, WORD xPos, WORD yPos) PURE;
+
+	//Window state callbacks
+
+	/* Called when the window is no longer visible. */
+	virtual VOID STDMETHODCALLTYPE OnWindowHide() PURE;
+
+	/* Called when the window is becoming visible. */
+	virtual VOID STDMETHODCALLTYPE OnWindowShow() PURE;
+
+	/* Called when the window is closing. */
+	virtual VOID STDMETHODCALLTYPE OnWindowClose() PURE;
+
+	//Back buffer callbacks
+
+	/* Called when a new back buffer is being created. */
+	virtual VOID STDMETHODCALLTYPE OnBackBufferCreate() PURE;
+
+	/* Called when the back buffer is being released on resize */
+	virtual VOID STDMETHODCALLTYPE OnBackBufferRelease() PURE;
 };
 
 struct __declspec(uuid("20203c63-f6f4-47ea-93cd-2784f02ecd61")) IDXWindow : public IUnknown {
@@ -54,6 +100,8 @@ struct __declspec(uuid("20203c63-f6f4-47ea-93cd-2784f02ecd61")) IDXWindow : publ
 	virtual DXWINDOW_STATE STDMETHODCALLTYPE GetState() PURE;
 
 	virtual VOID STDMETHODCALLTYPE SetState(DXWINDOW_STATE State) PURE;
+
+	virtual VOID STDMETHODCALLTYPE GetBackBuffer(REFIID riid, void** ppvBackBuffer) PURE;
 };
 
 #ifdef _DXWINDOW_DLL_PROJECT
