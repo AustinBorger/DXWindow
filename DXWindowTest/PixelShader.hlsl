c2774308000351bc741b1aeeaa0a9888c0a6958f
Austin Borger <aaborger@gmail.com>
2015-12-07 14:00:45 -0500

    Fixed pixel shader warning

--- DXWindowTest/PixelShader.hlsl
+++ DXWindowTest/PixelShader.hlsl
@@ -3,7 +3,7 @@ struct VSOut {
 	float2 uv : TexCoord;
 };
 
-static const float pi = 3.14159;
+static const float pi = 3.14159f;
 const float time;
 
 float sinPragma(float pos) {
@@ -12,12 +12,12 @@ float sinPragma(float pos) {
 
 float4 main(VSOut input) : SV_Target {
 	float2 p = input.uv;
-	float2 q = p - float2(0.5 + 0.1 * sin(time * pi), 0.5);
+	float2 q = p - float2(0.5f + 0.1f * sin(time * pi), 0.5f);
 
-	float3 col = float3(p, 1.0);
+	float3 col = float3(p, 1.0f);
 
-	float z = 0.2 + 0.1 * cos(time * pi * 2.0) + 0.01 * sin(time * pi * 8.0) * 2 * cos(atan(q) * (80.0 * sinPragma(pi * time) + 40.0));
-	col *= smoothstep(z, z + 0.01, length(q));
+	float z = 0.2f + 0.1f * cos(time * pi * 2.0) + 0.01f * sin(time * pi * 8.0) * 2.0f * cos(atan(q.x) * (80.0f * sinPragma(pi * time) + 40.0f));
+	col *= smoothstep(z, z + 0.01f, length(q));
 
-	return float4(col, 1.0);
+	return float4(col, 1.0f);
 }
